6ab9dbfa8edaa1f2568610682ec4e7da066e1641
themorning <themorning84@gmail.com>
2020-02-06 22:55:52 +0900

    reflection get과 set

---
+++ src/main/java/com/eeswan/modify/App.java
@@ -0,0 +1,46 @@
+package com.eeswan.modify;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class App {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException {
+
+        Class<?> bookClass = Class.forName("com.eeswan.modify.Book");
+
+        Constructor<?> constructor = bookClass.getConstructor(null);
+        Book book = (Book) constructor.newInstance();
+        System.out.println(book);
+
+        Constructor<?> constructor1 = bookClass.getConstructor(String.class);
+        Book book1 = (Book) constructor1.newInstance("myBook");
+        System.out.println(book1);
+
+        Field a = Book.class.getDeclaredField("A");
+        System.out.println( a.get(null));
+        a.set(null, "AAA");
+        System.out.println(a.get(null));
+
+        Field b = Book.class.getDeclaredField("B");
+        b.setAccessible(true);
+        System.out.println(b.get(book));
+        b.set(book, "BBBB");
+        System.out.println(b.get(book));
+
+
+        Method c = Book.class.getDeclaredMethod("c");
+        c.setAccessible(true);
+        c.invoke(book);
+
+        Method d = Book.class.getDeclaredMethod("sum", int.class, int.class);
+        int invoke = (int) d.invoke(book, 1, 2);
+        System.out.println(invoke);
+
+
+
+
+
+    }
+}
---
+++ src/main/java/com/eeswan/modify/Book.java
@@ -0,0 +1,24 @@
+package com.eeswan.modify;
+
+public class Book {
+
+    public static String A ="A";
+
+    private String B = "B";
+
+    public Book() {
+
+    }
+
+    public Book(String b) {
+        B = b;
+    }
+
+    private void c() {
+        System.out.println("C");
+    }
+
+    public int sum(int left, int right) {
+        return left + right;
+    }
+}
