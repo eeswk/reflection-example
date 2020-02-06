a6307d16a37f8c901c1c20eb2d138a392a996764
themorning <themorning84@gmail.com>
2020-02-06 23:46:40 +0900

    new Inject Annotation make

---
+++ src/main/java/com/eeswan/di/BookRepository.java
@@ -0,0 +1,5 @@
+package com.eeswan.di;
+
+public class BookRepository {
+
+}
---
+++ src/main/java/com/eeswan/di/BookService.java
@@ -0,0 +1,8 @@
+package com.eeswan.di;
+
+public class BookService {
+
+    @Inject
+    BookRepository bookRepository;
+
+}
